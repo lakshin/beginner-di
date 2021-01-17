7d22e7691688a6f6e47d9d7edf1ec3211cbb044f
Lakshin Karunaratne <LakshinK@99x.lk>
2021-01-17 16:24:44 +0530

    Creating CSV Reader project
    (cherry picked from commit cab43192e56f65a7d9a4bae462e4dcf566f293b1)

---
+++ PlanetApp/PlanetDataReader.CSV/CSVLoader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetDataReader.CSV
+{
+	public class CSVLoader : ICSVLoader
+	{
+		private string _csvPath;
+
+		public CSVLoader(string csvPath)
+		{
+			if (string.IsNullOrEmpty(csvPath)) throw new ArgumentNullException(nameof(csvPath));
+			_csvPath = csvPath;
+		}
+
+		public async Task<string> LoadCSV()
+		{
+			using (var reader = new StreamReader(_csvPath))
+			{
+				return await reader.ReadToEndAsync();
+			}
+		}
+	}
+}
