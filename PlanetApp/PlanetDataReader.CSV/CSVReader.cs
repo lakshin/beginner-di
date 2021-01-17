cab43192e56f65a7d9a4bae462e4dcf566f293b1
Lakshin Karunaratne <LakshinK@99x.lk>
2021-01-17 16:24:44 +0530

    Creating CSV Reader project

---
+++ PlanetApp/PlanetDataReader.CSV/CSVReader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PlanetApp.Common;
+
+namespace PlanetDataReader.CSV
+{
+	public class CSVReader : IPlanetReader
+	{
+		private readonly ICSVLoader _csvLoader;
+
+		public CSVReader()
+		{
+			var filePath = $"{AppDomain.CurrentDomain.BaseDirectory}Data/Planets.csv";
+			_csvLoader = new CSVLoader(filePath);
+		}
+
+		public async Task<IReadOnlyCollection<Planet>> GetPlanets()
+		{
+			var fileData = await _csvLoader.LoadCSV();
+			var planets = ParseCSV(fileData);
+			return planets;
+		}
+
+		private IReadOnlyCollection<Planet> ParseCSV(string csvData)
+		{
+			var planets = new List<Planet>();
+			var lines = csvData.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+			foreach (string line in lines)
+			{
+				try
+				{
+					planets.Add(ParsePlanetString(line));
+				}
+				catch (Exception)
+				{
+					// Skip the bad record, log it, and move to the next record
+					//TODO: good practice to log them
+				}
+			}
+			return planets;
+		}
+
+		private Planet ParsePlanetString(string planetData)
+		{
+			var elements = planetData.Split(',');
+			var planet = new Planet
+			{
+				Id = int.Parse(elements[0]),
+				Name = elements[1],
+				ImageUri = elements[2]
+			};
+
+			return planet;
+		}
+	}
+}
